9456fbded3f2362e8e96b6f89ae44d1aab47eb08
LeaoMartelo <leaomartelo@gmail.com>
2024-09-12 00:53:54 -0300

    raylib fun

--- raylib_tests/draggable_square/game.c
+++ raylib_tests/draggable_square/game.c
@@ -3,10 +3,12 @@
 #include <stdio.h>
 
 #define MAX_FPS 100
-#define WIDTH 800
+#define WIDTH 600
 #define HEIGHT 600
 #define SIZE 100
 #define SPEED 10
+
+#define TEXT "bostenga"
 
 int main() {
 
@@ -14,14 +16,6 @@ int main() {
     /*MaximizeWindow();*/
     InitWindow(WIDTH, HEIGHT, "Raylib");
     /*SetTargetFPS(MAX_FPS);*/
-
-    /*start ball in the middle of the screen*/
-    int x = WIDTH / 2;
-    int y = HEIGHT / 2;
-
-    /*int radius = SIZE / 2;*/
-    /*int speed_x = SPEED;*/
-    /*int speed_y = SPEED;*/
 
     Rectangle square = {
         .x = (float)WIDTH / 2,
@@ -37,35 +31,11 @@ int main() {
         float deltaTime = GetFrameTime();
         DrawFPS(1, 1);
 
-        bool hit_sides = false;
-        bool hit_up_down = false;
-
         BeginDrawing();
 
         {
-            /*if (x - radius <= 0 || x + radius >= GetScreenWidth()) {
-                speed_x = -1 * speed_x;
-                x = x + speed_x;
-                hit_sides = true;
-            }
-
-            if (y - radius <= 0 || y + radius >= GetScreenHeight()) {
-                speed_y = -1 * speed_y;
-                y = y + speed_y;
-                hit_up_down = true;
-            }
-
-            if (hit_sides || hit_up_down) {
-                //radius++;
-
-                //ClearBackground(BLACK);
-            }
-            x = x + speed_x;
-            y = y + speed_y;
-
-                DrawCircle(x, y, radius, WHITE);*/
-
             DrawRectangle(square.x, square.y, SIZE, SIZE, GREEN);
+            DrawText(TEXT, square.x + (float)SIZE / 4, square.y + ((float)SIZE / 2), 12, WHITE);
             Vector2 mouse_pos = GetMousePosition();
 
             if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
@@ -73,7 +43,7 @@ int main() {
                 printf("mouse: {x:%f, y:%f} \n", mouse_pos.x, mouse_pos.y);
 
                 if (CheckCollisionPointRec(mouse_pos, square)) {
-                    printf("is inside square!\n");
+                    // printf("is inside square!\n");
                     square.x = mouse_pos.x - (float)SIZE / 2;
                     square.y = mouse_pos.y - (float)SIZE / 2;
                 }
