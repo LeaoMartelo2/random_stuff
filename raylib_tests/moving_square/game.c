795882312eb6c44d9c701a36e3637a114e9c2bef
LeaoMartelo <leaomartelo@gmail.com>
2024-09-11 23:57:12 -0300

    rename

--- raylib_tests/moving_square/game.c
+++ raylib_tests/moving_square/game.c
@@ -1,29 +1,34 @@
 #include <raylib.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-#define MAX_FPS 60
+#define MAX_FPS 100
 #define WIDTH 800
 #define HEIGHT 600
-#define SIZE 30
+#define SIZE 100
 #define SPEED 10
 
 int main() {
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    MaximizeWindow();
-    InitWindow(GetScreenWidth(), GetScreenHeight(), "Bouncing ball");
-    SetTargetFPS(MAX_FPS);
+    /*MaximizeWindow();*/
+    InitWindow(WIDTH, HEIGHT, "Raylib");
+    /*SetTargetFPS(MAX_FPS);*/
 
     /*start ball in the middle of the screen*/
     int x = WIDTH / 2;
     int y = HEIGHT / 2;
 
-    int radius = SIZE / 2;
-    int speed_x = SPEED;
-    int speed_y = SPEED;
+    /*int radius = SIZE / 2;*/
+    /*int speed_x = SPEED;*/
+    /*int speed_y = SPEED;*/
 
-    int rect_x = WIDTH / 1;
-    int rect_y = HEIGHT / 1;
+    Rectangle square = {
+        .x = (float)WIDTH / 2,
+        .y = (float)HEIGHT / 2,
+        .width = SIZE,
+        .height = SIZE,
+    };
 
     while (!WindowShouldClose()) {
 
@@ -38,7 +43,7 @@ int main() {
         BeginDrawing();
 
         {
-            if (x - radius <= 0 || x + radius >= GetScreenWidth()) {
+            /*if (x - radius <= 0 || x + radius >= GetScreenWidth()) {
                 speed_x = -1 * speed_x;
                 x = x + speed_x;
                 hit_sides = true;
@@ -51,21 +56,27 @@ int main() {
             }
 
             if (hit_sides || hit_up_down) {
-                /*radius++;*/
+                //radius++;
 
-                /*ClearBackground(BLACK);*/
+                //ClearBackground(BLACK);
             }
             x = x + speed_x;
             y = y + speed_y;
 
-            /*DrawCircle(x, y, radius, WHITE);*/
+                DrawCircle(x, y, radius, WHITE);*/
 
-            DrawRectangle(rect_x, rect_y, SIZE * 2, SIZE * 2, GREEN);
+            DrawRectangle(square.x, square.y, SIZE, SIZE, GREEN);
             Vector2 mouse_pos = GetMousePosition();
 
             if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-                rect_x = mouse_pos.x;
-                rect_y = mouse_pos.y;
+
+                printf("mouse: {x:%f, y:%f} \n", mouse_pos.x, mouse_pos.y);
+
+                if (CheckCollisionPointRec(mouse_pos, square)) {
+                    printf("is inside square!\n");
+                    square.x = mouse_pos.x - (float)SIZE / 2;
+                    square.y = mouse_pos.y - (float)SIZE / 2;
+                }
             }
         }
         EndDrawing();
