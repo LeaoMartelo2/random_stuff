f9df1358030b5894439b0e51647968408d720d28
LeaoMartelo <leaomartelo@gmail.com>
2024-09-14 12:25:09 -0300

    thirty

--- raylib_tests/draggable_square/game.c
+++ raylib_tests/draggable_square/game.c
@@ -1,20 +1,56 @@
 #include <raylib.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-#define MAX_FPS 100
-#define WIDTH 800
+#define MAX_FPS 1000
+#define WIDTH 900
 #define HEIGHT 600
 #define SIZE 100
 #define SPEED 10
+#define MAX_SIZE 501
 
 #define TEXT "hi"
 
+void resize_square(Rectangle *square) {
+
+    float deltaTime = GetFrameTime();
+
+    /*printf("square {width:%f, height:%f} \n", square->width, square->height);*/
+
+    if (IsKeyDown(KEY_S)) {
+        if (square->height + 1 <= MAX_SIZE) {
+            square->height = square->height + GetFPS() * deltaTime;
+        }
+    }
+
+    if (IsKeyDown(KEY_W)) {
+        if (square->height - 1 >= 100) {
+            square->height = square->height - GetFPS() * deltaTime;
+        }
+    }
+
+    if (IsKeyDown(KEY_D)) {
+        if (square->width + 1 <= MAX_SIZE) {
+            square->width = square->width + GetFPS() * deltaTime;
+        }
+    }
+
+    if (IsKeyDown(KEY_A)) {
+        if (square->width - 1 >= 100) {
+            square->width = square->width - GetFPS() * deltaTime;
+        }
+    }
+}
+
 int main() {
 
-    /*SetConfigFlags(FLAG_WINDOW_RESIZABLE);*/
-    /*MaximizeWindow();*/
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(WIDTH, HEIGHT, "Raylib");
-    /*SetTargetFPS(MAX_FPS);*/
+    SetTargetFPS(MAX_FPS);
+    InitAudioDevice();
+
+    Sound button_snd = LoadSound("resources/sound.mp3");
+    Texture2D thirty = LoadTexture("resources/image.png");
 
     Rectangle square = {
         .x = (float)WIDTH / 2,
@@ -23,45 +59,70 @@ int main() {
         .height = 100,
     };
 
+    Rectangle button = {
+        .x = (float)WIDTH - thirty.width - 5,
+        .y = (float)5,
+        .width = thirty.width,
+        .height = thirty.height,
+    };
+    bool button_action = false;
+
     while (!WindowShouldClose()) {
 
-        ClearBackground(BLANK);
+        ClearBackground(BLACK);
 
+        Vector2 mouse_pos = GetMousePosition();
         float deltaTime = GetFrameTime();
         DrawFPS(1, 1);
 
-        BeginDrawing();
+        resize_square(&square);
 
-        {
-            if (IsKeyDown(KEY_A)) {
-                if (square.width - 10 > 0) {
-                    square.width = square.width - 1000 * deltaTime;
-                    square.height = square.height - 1000 * deltaTime;
-                }
-            }
+        Vector2 square_pos = {square.x, square.y};
+        Vector2 square_size = {square.width, square.height};
 
-            if (IsKeyDown(KEY_D)) {
-                if (square.height + 10 < 500) {
-                    square.width = square.width + 1000 * deltaTime;
-                    square.height = square.height + 1000 * deltaTime;
-                }
-            }
+        button_action = false;
 
-            DrawRectangle(square.x, square.y, square.width, square.height, GREEN);
-            DrawText(TEXT, square.x + (square.width / 2) - 1, square.y + square.height / 2, 20, WHITE);
-            Vector2 mouse_pos = GetMousePosition();
+        /*printf("mouse: {x:%f, y:%f} \n", mouse_pos.x, mouse_pos.y);*/
+
+        // draggable square
+        if (CheckCollisionPointRec(mouse_pos, square)) {
 
             if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-                /*printf("mouse: {x:%f, y:%f} \n", mouse_pos.x, mouse_pos.y);*/
-
-                if (CheckCollisionPointRec(mouse_pos, square)) {
-                    /*printf("is inside square!\n");*/
-                    square.x = mouse_pos.x - square.width / 2;
-                    square.y = mouse_pos.y - square.height / 2;
-                }
+                SetMouseCursor(MOUSE_CURSOR_RESIZE_ALL);
+                /*printf("is inside square!\n");*/
+                square.x = mouse_pos.x - square.width / 2;
+                square.y = mouse_pos.y - square.height / 2;
+            } else {
+                SetMouseCursor(MOUSE_CURSOR_DEFAULT);
             }
+        }
+
+        // snd button
+
+        if (CheckCollisionPointRec(mouse_pos, button)) {
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                button_action = true;
+            }
+        }
+
+        if (button_action) {
+            /*printf("audio played\n");*/
+            PlaySound(button_snd);
+        }
+
+        BeginDrawing();
+        {
+            // snd button
+            DrawRectangle(button.x, button.y, button.width, button.height, RED);
+            DrawTexture(thirty, button.x, button.y, WHITE);
+
+            // draggable square
+            DrawRectangleV(square_pos, square_size, GREEN);
+            DrawText(TEXT, square.x + (square.width / 2) - 2, square.y + (square.height / 2), 20, WHITE);
         }
         EndDrawing();
     }
+    UnloadTexture(thirty);
+    CloseWindow();
     return 0;
 }
